a3a2ec45ae2831a48bdc9b6cda4b13190dc31b33
wujohns <wujohns@163.com>
2023-08-25 18:47:48 +0800

    相关进度推进

--- sd-api-test/2.4.txt2img_plot.js
+++ sd-api-test/2.4.txt2img_plot.js
@@ -1,5 +1,5 @@
 /**
- * sd 文生图部分脚本测试
+ * sd 文生图部分 x/y/z plot脚本 测试
  * 参考: https://github.com/mix1009/sdwebuiapi/commit/fe269dc2d4f8a98e96c63c8a7d3b5f039625bc18
  *
  * @author nobody
@@ -11,6 +11,24 @@ const { convertImg } = require('../libs/utils')
 const { txt2img } = require('../libs/sd_api')
 
 const run = async () => {
-  // TODO
+  const resData = await txt2img({
+    prompt: '1girl',
+    steps: 20,
+    seed: 34324235,
+    override_settings: {
+      sd_model_checkpoint: 'darkSushiMixMix_225D.safetensors [cca17b08da]',   // 基础模型配置
+      sd_vae: 'animevae.pt',        // vae 配置
+      CLIP_stop_at_last_layers: 1,  // clip skip 设置
+    },
+    script_name: 'X/Y/Z plot',    // 目前推测名称来自 title
+    script_args: [                // 目前推测参数对应关系为 run 函数中的参数
+      // TODO
+    ]
+  })
+  const fileName = '2.4.txt2img_plot.png'
+  const savePath = path.join(__dirname, `../temp/${ fileName }`)
+  fs.removeSync(savePath)
+
+  convertImg(resData.images[0], savePath)
 }
 run().then(() => process.exit(0))
